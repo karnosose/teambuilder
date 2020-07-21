3b4da9a965af15d279ffc83e4b80556edf556b35
Karno Sose <karnosose@gmail.com>
2020-07-21 20:11:24 +0400

    remove unnecessary files

--- src/constants/menuItems.js
+++
@@ -1,7 +0,0 @@
-export const MENU_ITEMS = [
-    {url: '/teams',title:'Teams'},
-    {url: '/topics', title: 'Topics'},
-    {url: '/projects', title: 'Projects'},
-    {url: '/', title: 'Log out'},
-    {url: '/profile', title: 'Profile'}
-];
