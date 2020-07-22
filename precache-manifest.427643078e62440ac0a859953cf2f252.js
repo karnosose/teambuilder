f70440db8252baf9978f50acc46b4f811557fce9
Karno Sose <karnosose@gmail.com>
2020-07-22 09:28:15 +0400

    Updates

---
+++ precache-manifest.427643078e62440ac0a859953cf2f252.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "613f3a0d4f205812fc50",
+    "url": "/karnosose/teambuilder.git/static/js/runtime~main.613f3a0d.js"
+  },
+  {
+    "revision": "9ee4b99abe3ceee8d55c",
+    "url": "/karnosose/teambuilder.git/static/js/main.9ee4b99a.chunk.js"
+  },
+  {
+    "revision": "f321206e29b45d6f600e",
+    "url": "/karnosose/teambuilder.git/static/js/1.f321206e.chunk.js"
+  },
+  {
+    "revision": "9ee4b99abe3ceee8d55c",
+    "url": "/karnosose/teambuilder.git/static/css/main.3528be30.chunk.css"
+  },
+  {
+    "revision": "f321206e29b45d6f600e",
+    "url": "/karnosose/teambuilder.git/static/css/1.3f84fde3.chunk.css"
+  },
+  {
+    "revision": "21b6dcf91572dd74d2fe857a79b581c7",
+    "url": "/karnosose/teambuilder.git/index.html"
+  }
+];
